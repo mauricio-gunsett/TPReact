49a370305742ac521b2b868e83acc3d5e79e31f4
Alexis Mauricio Gunsett <mau_gunsett@hotmail.com>
2023-09-30 18:45:17 -0300

    realizo ejercicio 1

--- alexis/src/main.jsx
+++ alexis/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
 import './index.css'
+import Ejercicio1 from './pages/Ejercicio1.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Ejercicio1 />
   </React.StrictMode>,
 )
--- alexis/src/pages/Ejercicio1.jsx
+++ alexis/src/pages/Ejercicio1.jsx
@@ -1,6 +1,6 @@
 const Ejercicio1 = () => {
   return (
-    <h1>Hola mundo</h1>
+    <h1 className="container">Hola mundo</h1>
   )
 }
 export default Ejercicio1
